e56e3b2cfb20d74bc2873e234d6c7f2d00ec3691
ines-mouandjolobe <inesmouandjolobe@gmail.com>
2020-12-04 16:05:36 +0100

    router

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -6,7 +6,7 @@ import {
 } from "react-router-dom";
 import App from './App';
 
-export default function BasicExample() {
+const AppRoutes = () => {
   return (
     <Router>
         <Switch>
@@ -17,3 +17,5 @@ export default function BasicExample() {
     </Router>
   );
 }
+
+export default AppRoutes;
